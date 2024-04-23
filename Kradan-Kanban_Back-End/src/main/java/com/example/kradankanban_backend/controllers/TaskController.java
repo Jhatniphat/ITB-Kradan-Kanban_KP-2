c6e081ce6feb95d49f02fd61f6982a5d2f349309
OParnu <parnuwat8049@gmail.com>
2024-04-23 22:30:01 +0700

    DTO

--- Kradan-Kanban_Back-End/src/main/java/com/example/kradankanban_backend/controllers/TaskController.java
+++ Kradan-Kanban_Back-End/src/main/java/com/example/kradankanban_backend/controllers/TaskController.java
@@ -1,22 +1,34 @@
 package com.example.kradankanban_backend.controllers;
 
+import com.example.kradankanban_backend.dtos.TaskDTO;
 import com.example.kradankanban_backend.entities.TaskEntity;
 import com.example.kradankanban_backend.services.TaskService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/Tasks")
 public class TaskController {
     @Autowired
     private TaskService service;
+    @Autowired
+    private ModelMapper modelMapper;
 
-    @GetMapping("")
-    public List<TaskEntity> findAll(){
-        return service.findAll();
+    //    @GetMapping("")
+//    public List<TaskEntity> findAll(){
+//        return service.findAll();
+//    }
+    @GetMapping("") //DTO
+    public ResponseEntity<Object> getAllTasks() {
+        List<TaskEntity> tasks = service.findAll();
+        List<TaskDTO> taskDTOS = tasks.stream().map(p -> modelMapper.map(p, TaskDTO.class)).collect(Collectors.toList());
+        return ResponseEntity.ok(taskDTOS);
     }
 }
--- Kradan-Kanban_Back-End/src/main/java/com/example/kradankanban_backend/services/TaskService.java
+++ Kradan-Kanban_Back-End/src/main/java/com/example/kradankanban_backend/services/TaskService.java
@@ -1,10 +1,13 @@
 package com.example.kradankanban_backend.services;
 
+import com.example.kradankanban_backend.dtos.TaskDTO;
 import com.example.kradankanban_backend.entities.TaskEntity;
 import com.example.kradankanban_backend.repositories.TaskRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
