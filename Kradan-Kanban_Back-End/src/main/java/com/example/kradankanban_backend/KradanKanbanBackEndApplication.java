6c41f835759673f0eb0c0d8936f284a31aca9d02
Jhatniphat Sarakal <jhatniphat.sara@kmutt.ac.th>
2024-04-23 14:13:04 +0700

    Base Project
    - Create Vue Proj
    - Create SpringBoot Proj

---
+++ Kradan-Kanban_Back-End/src/main/java/com/example/kradankanban_backend/KradanKanbanBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.kradankanban_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KradanKanbanBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KradanKanbanBackEndApplication.class, args);
+    }
+
+}
---
+++ Kradan-Kanban_Back-End/src/test/java/com/example/kradankanban_backend/KradanKanbanBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kradankanban_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KradanKanbanBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
