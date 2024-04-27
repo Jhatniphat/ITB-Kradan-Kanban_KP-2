846bc1c31bf0fe37afd7274aeec5c182f67f4b8e
DekchaiHeart <110713141+DekchaiHeart@users.noreply.github.com>
2024-04-28 00:49:54 +0700

    Add class itbkk-*

--- Kradan-Kanban_Front-End/src/views/TasklistView.vue
+++ Kradan-Kanban_Front-End/src/views/TasklistView.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { onBeforeMount, ref , watch} from "vue";
-import { useRoute } from 'vue-router'
+import { onBeforeMount, ref, watch } from "vue";
+import { useRoute } from "vue-router";
 import Taskdetail from "../components/Taskdetail.vue";
 import Tasktable from "../components/Tasktable.vue";
 import { getAllTasks, getTaskById } from "../lib/fetchUtils.js";
@@ -9,42 +9,40 @@ import router from "@/router";
 const showModal = ref(false);
 const route = useRoute();
 
-const loading = ref(false)
-const allTasks = ref(null)
-const error = ref(null)
-const selectedTaskId = ref(0)
+const loading = ref(false);
+const allTasks = ref(null);
+const error = ref(null);
+const selectedTaskId = ref(0);
 
 const openModal = (taskId) => {
-  showModal.value = true
-  selectedTaskId.value = taskId
+  showModal.value = true;
+  selectedTaskId.value = taskId;
 };
 
 async function fetchData(id) {
-  if(id !== undefined) {
-    openModal(id)
+  if (id !== undefined) {
+    openModal(id);
   }
-  error.value = allTasks.value = null
-  loading.value = true
+  error.value = allTasks.value = null;
+  loading.value = true;
   try {
     // replace `getPost` with your data fetching util / API wrapper
-    allTasks.value = await getAllTasks()
+    allTasks.value = await getAllTasks();
   } catch (err) {
-    error.value = err.toString()
+    error.value = err.toString();
   } finally {
-    loading.value = false
+    loading.value = false;
   }
 }
 
-watch(() => route.params.id, fetchData, { immediate: true })
+watch(() => route.params.id, fetchData, { immediate: true });
 
 onBeforeMount(() => {
   if (route.params.id !== undefined) {
-    selectedTaskId.value = parseInt(route.params.id)
-    showModal.value = true
+    selectedTaskId.value = parseInt(route.params.id);
+    showModal.value = true;
   }
-})
-
-
+});
 </script>
 
 <template>
@@ -55,8 +53,7 @@ onBeforeMount(() => {
   <!-- Table -->
   <div class="flex flex-col">
     <h1 class="mb-4 text-center text-2xl font-semibold italic">Task Listing</h1>
-    <table
-      class="table table-lg">
+    <table class="table table-lg">
       <!-- head -->
       <thead>
         <tr>
@@ -70,28 +67,38 @@ onBeforeMount(() => {
         <!-- Listing -->
         <tr v-if="allTasks === null">
           <td colspan="4">Waiting For Data</td>
-        </tr> 
-        <tr v-if="allTasks !== null" v-for="task in allTasks" :key="task.taskId" class="hover">
+        </tr>
+        <tr
+          v-if="allTasks !== null"
+          v-for="task in allTasks"
+          :key="task.taskId"
+          class="itbkk-item hover"
+        >
           <th>{{ task.taskId }}</th>
-          <td>
+          <td class="itbkk-title">
             <!-- <RouterLink :to="`/task/${task.taskId}`"> -->
             <button @click="router.push(`/task/${task.taskId}`)">
               {{ task.taskTitle }}
             </button>
-          <!-- </RouterLink> -->
+            <!-- </RouterLink> -->
           </td>
-          <td>{{ task.taskAssignees }}</td>
-          <td>{{ task.taskStatus }}</td>
+          <td class="itbkk-assignees">{{ task.taskAssignees }}</td>
+          <td class="itbkk-status">{{ task.taskStatus }}</td>
         </tr>
-
       </tbody>
     </table>
     <!-- <Tasktable/> -->
   </div>
   <!-- Modal -->
   <Teleport to="#modal">
-    <div v-if="showModal" class="absolute left-0 right-0 z-50 top-20 m-auto w-2/5 h-2/3 bg-slate-50 rounded-lg">
-      <Taskdetail :taskId="parseInt( selectedTaskId )" @closeModal="showModal = false" />
+    <div
+      v-if="showModal"
+      class="absolute left-0 right-0 z-50 top-20 m-auto w-2/5 h-2/3 bg-slate-50 rounded-lg"
+    >
+      <Taskdetail
+        :taskId="parseInt(selectedTaskId)"
+        @closeModal="showModal = false"
+      />
     </div>
   </Teleport>
 </template>
