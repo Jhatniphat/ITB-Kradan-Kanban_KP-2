80af6930bab88286f27bef411fc9ca72af6600d7
Jhatniphat Sarakal <52996903+Jhatniphat@users.noreply.github.com>
2024-05-01 15:28:46 +0700

    Update SampleData.sql

--- Database/SampleData.sql
+++ Database/SampleData.sql
@@ -3,5 +3,5 @@ insert into task (id, title , description , assignees , status) values ( 1 , 'Ta
 , 'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5' 
 , 'Assignees1Assignees2Assignees3' , 'No Status');
 insert into task (id, title , description , assignees , status) values ( 2 , 'Repository' , '' , '' , 'To Do');
-insert into task (id, title , description , assignees , status) values ( 3 , 'ดาต้าเบส' , 'ສ້າງຖານຂໍ້ ມູ ນ' , 'あなた、彼彼⼥(私ではありません)' , 'Doing');
-insert into task (id, title , description , assignees , status) values ( 4 , '_Infrastructure_' , '_Setup containers_' , ' ไก่งวงกับเพนกวิน ' , 'Done');
+insert into task (id, title , description , assignees , status) values ( 3 , 'ดาต้าเบส' , 'ສ້າງຖານຂໍ້ ມູ ນ' , 'あなた、彼、彼女 (私ではありません)' , 'Doing');
+insert into task (id, title , description , assignees , status) values ( 4 , '_Infrastructure_' , '_Setup containers_' , ' ไก่งวง กับ เพนกวิน ' , 'Done');
