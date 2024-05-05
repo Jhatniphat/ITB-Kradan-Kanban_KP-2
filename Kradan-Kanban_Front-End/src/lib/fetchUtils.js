056d7065a9daf4cb5f129e4f5ef8aebf2cb1d9a0
Jhatniphat Sarakal <jhatniphat.sara@kmutt.ac.th>
2024-05-05 21:14:28 +0700

    format data for status

--- Kradan-Kanban_Front-End/src/lib/fetchUtils.js
+++ Kradan-Kanban_Front-End/src/lib/fetchUtils.js
@@ -11,6 +11,9 @@ export async function getAllTasks() {
       method: "GET",
     }); //GET Method
     const items = await res.json();
+    items.forEach(item => {
+      item.status = ENUMToTitleCase(item.status)
+    });
     return items;
   } catch (error) {}
 }
@@ -25,6 +28,7 @@ export async function getTaskById(id) {
       item = await res.json();
       item.createdOn = timeFormater(item.createdOn);
       item.updatedOn = timeFormater(item.updatedOn);
+      item.status = ENUMToTitleCase(item.status)
       return item;
     } else {
       return res.status;
@@ -35,8 +39,9 @@ export async function getTaskById(id) {
 }
 
 export async function addTask(newTask) {
+  newTask.status = titleCaseToENUM(newTask.status)
   let res, item;
-  console.log(JSON.stringify({ ...newTask }));
+  // console.log(JSON.stringify({ ...newTask }));
   try {
     res = await fetch(`${import.meta.env.VITE_BASE_URL}/tasks`, {
       method: "POST",
@@ -47,7 +52,7 @@ export async function addTask(newTask) {
     });
     if (res.status === 201) {
       item = await res.json();
-      console.log(res.status);
+      item.status = ENUMToTitleCase(item.status)
       return item;
     } else {
       return res.status;
@@ -79,6 +84,7 @@ export async function addTask(newTask) {
 // }
 
 export async function editTask(id, Task) {
+  Task.status = titleCaseToENUM(Task.status)
   try {
     const res = await fetch(`${import.meta.env.VITE_BASE_URL}/tasks/${id}`, {
       method: "PUT",
@@ -90,6 +96,7 @@ export async function editTask(id, Task) {
 
     if (res.ok) {
       const updatedTask = await res.json();
+      updatedTask.status = ENUMToTitleCase(updatedTask.status)
       return updatedTask;
     } else {
       throw new Error(`Failed to update task: ${res.status}`);
@@ -106,7 +113,7 @@ export async function deleteTask(id) {
     });
     if (res.ok) {
       const item = await res.json();
-      console.log(res.status);
+      item.status = ENUMToTitleCase(item.status)
       return item;
     } else {
       return res.status;
@@ -137,3 +144,20 @@ function timeFormater(time) {
     timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
   });
 }
+
+function ENUMToTitleCase(str){
+  const words = str.split("_");
+  const titleCaseWords = words.map((word) => {
+    return word[0].toUpperCase() + word.slice(1).toLowerCase();
+  });
+  return titleCaseWords.join(" ");
+};
+
+function titleCaseToENUM(str){
+  return str.split(" ").join("_").toUpperCase()
+};
+
+// console.log(titleCaseToENUM('No Status'))
+// console.log(ENUMToTitleCase('NO_STATUS'))
+
+
