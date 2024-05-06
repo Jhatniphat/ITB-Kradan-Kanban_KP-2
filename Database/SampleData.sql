2cd4116d82c077ef795a4d253c8898e073063248
DekchaiHeart <110713141+DekchaiHeart@users.noreply.github.com>
2024-05-06 18:21:12 +0700

    Cypress Sprint 2

--- Database/SampleData.sql
+++ Database/SampleData.sql
@@ -1,7 +1,7 @@
 use `intergrate-kp-2`;
 insert into task (id, title , description , assignees , status, createdOn, updatedOn) values ( 1 , 'TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0' 
 , 'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5' 
-, 'Assignees1Assignees2Assignees3' , 'No Status', '2024-04-22 09:00:00', '2024-04-22 09:00:00');
-insert into task (id, title , description , assignees , status, createdOn, updatedOn) values ( 2 , 'Repository' , '' , '' , 'To Do', '2024-04-22 09:05:00', '2024-04-22 14:00:00');
-insert into task (id, title , description , assignees , status, createdOn, updatedOn) values ( 3 , 'ดาต้าเบส' , 'ສ້າງຖານຂໍ້ມູນ' , 'あなた、彼、彼女 (私ではありません)' , 'Doing', '2024-04-22 09:10:00', '2024-04-25 00:00:00');
-insert into task (id, title , description , assignees , status, createdOn, updatedOn) values ( 4 , '_Infrastructure_' , '_Setup containers_' , ' ไก่งวง กับ เพนกวิน ' , 'Done', '2024-04-22 09:15:00', '2024-04-22 10:00:00');
+, 'Assignees1Assignees2Assignees3' , 'NO_STATUS', '2024-04-22 09:00:00', '2024-04-22 09:00:00');
+insert into task (id, title , description , assignees , status, createdOn, updatedOn) values ( 2 , 'Repository' , '' , '' , 'TO_DO', '2024-04-22 09:05:00', '2024-04-22 14:00:00');
+insert into task (id, title , description , assignees , status, createdOn, updatedOn) values ( 3 , 'ดาต้าเบส' , 'ສ້າງຖານຂໍ້ມູນ' , 'あなた、彼、彼女 (私ではありません)' , 'DOING', '2024-04-22 09:10:00', '2024-04-25 00:00:00');
+insert into task (id, title , description , assignees , status, createdOn, updatedOn) values ( 4 , '_Infrastructure_' , '_Setup containers_' , ' ไก่งวง กับ เพนกวิน ' , 'DONE', '2024-04-22 09:15:00', '2024-04-22 10:00:00');
