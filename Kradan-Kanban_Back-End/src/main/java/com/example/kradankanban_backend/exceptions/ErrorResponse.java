217f5ff8303ae6048d70feb293f5ad0e24b6127c
Jhatniphat Sarakal <jhatniphat.sara@kmutt.ac.th>
2024-05-28 21:28:50 +0700

    refactor : remove comment code

--- Kradan-Kanban_Back-End/src/main/java/com/example/kradankanban_backend/exceptions/ErrorResponse.java
+++ Kradan-Kanban_Back-End/src/main/java/com/example/kradankanban_backend/exceptions/ErrorResponse.java
@@ -19,18 +19,6 @@ import java.util.Objects;
 @RequiredArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse{
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss'Z'" , timezone = "UTC")
-//    private LocalDateTime timestamp;
-//    private int status;
-//    private String message;
-//    private String instance;
-//
-//    public ErrorResponse(int status, String message, String instance) {
-//        this.timestamp = LocalDateTime.now();
-//        this.status = status;
-//        this.message = message;
-//        this.instance = instance;
-//    }
 
     private final int status;
     private final String message;
